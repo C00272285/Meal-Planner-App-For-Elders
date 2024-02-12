fc2154148b16aec2e324aefbdd8cdde69142637c
C00272285 <C00272285@itcarlow.ie>
2024-02-12 15:08:47 +0000

    Updated

--- app/src/main/java/com/example/design/MainActivity.java
+++ app/src/main/java/com/example/design/MainActivity.java
@@ -1,83 +1,106 @@
 package com.example.design;
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
-public class MainActivity extends AppCompatActivity
-{
-    //  Variables to get the id's from the XML file
-    EditText breakfast, lunch, dinner;  //TextFields
-    Button submit;  //Button used for inserting
-    CalendarView calendarView;  //Calendar to select the date for the meal
-    DatabaseReference ref;  //Reference to the Firebase database
-    User user;  //Java class User that holds the setters and getters
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText dinner, lunch, breakfast;
+    private Calendar calendar;
+    private DatabaseReference ref;
+    private Button insert;
+    private User user;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //The XML id's are set to the variables
+        // Initialize views
+        textView = findViewById(R.id.textView);
+        ImageView previousButton = findViewById(R.id.previousButton);
+        ImageView nextButton = findViewById(R.id.nextButton);
+        dinner = findViewById(R.id.dinner);
         breakfast = findViewById(R.id.breakfast);
         lunch = findViewById(R.id.lunch);
-        dinner = findViewById(R.id.dinner);
-        calendarView = findViewById(R.id.calendarView2);
+        insert = findViewById(R.id.button);
+
+        // Initialize Calendar instance
+        calendar = Calendar.getInstance();
+
+        // Set initial text in TextView
+        updateTextView();
+
         user = new User();
+        ref = FirebaseDatabase.getInstance("https://mealplanner-a23cb-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("Meals");
 
-        // Connect to Firebase Realtime Database in the Belgium region
-        ref = FirebaseDatabase.getInstance("https://mealplanner-a23cb-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("User");
-
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
-        {
+        // Set click listeners for arrow buttons
+        previousButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth)
-            {
-                //Format the selected date
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-                String selectedDate = dateFormat.format(view.getDate());
-
-                //Set the selected date in the User object
-                user.setDate(selectedDate);
+            public void onClick(View v) {
+                // Clear text in EditText
+                dinner.setText("");
+                lunch.setText("");
+                breakfast.setText("");
+                calendar.add(Calendar.DAY_OF_MONTH, -1);
+                updateTextView();
             }
         });
 
-        submit = findViewById(R.id.insertdata);
-        submit.setOnClickListener(v ->
-        {
-            //Get the entered meal details
-            String breakfastText = breakfast.getText().toString().trim();
-            String lunchText = lunch.getText().toString().trim();
-            String dinnerText = dinner.getText().toString().trim();
-
-            //Set the meal details in the User object
-            user.setBreakfast(breakfastText);
-            user.setLunch(lunchText);
-            user.setDinner(dinnerText);
-
-            //Create a HashMap to specify the order of fields
-            //  "Still not in order as of the moment"
-            Map<String, Object> userData = new HashMap<>();
-            userData.put("Date", user.getDate());
-            userData.put("Breakfast", user.getBreakfast());
-            userData.put("Lunch", user.getLunch());
-            userData.put("Dinner", user.getDinner());
-
-            //Push the User object to the Firebase Realtime Database with specified order
-            ref.push().setValue(userData);
-
-            //Show a toast message indicating that the data has been sent
-            Toast.makeText(MainActivity.this, "Data has been sent", Toast.LENGTH_LONG).show();
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Clear text in EditText
+                dinner.setText("");
+                lunch.setText("");
+                breakfast.setText("");
+                calendar.add(Calendar.DAY_OF_MONTH, 1);
+                updateTextView();
+            }
         });
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String breakfastText = breakfast.getText().toString().trim();
+                String lunchText = lunch.getText().toString().trim();
+                String dinnerText = dinner.getText().toString().trim();
+                String dateText = textView.getText().toString().trim();
+
+                user.setDate(dateText);
+                user.setBreakfast(breakfastText);
+                user.setLunch(lunchText);
+                user.setDinner(dinnerText);
+
+                ref.push().setValue(user); // Pushing user object instead of meal
+
+                Toast.makeText(MainActivity.this, "Data has been sent", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    // Method to update TextView with current day
+    private void updateTextView() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM dd, yyyy", Locale.getDefault());
+        String formattedDate = dateFormat.format(calendar.getTime());
+        textView.setText(formattedDate);
     }
 }
